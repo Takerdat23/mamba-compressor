62ea763c08a0d6aa8700b17bfebca15ca87de79f
nganngants <nganngants@gmail.com>
2025-03-31 04:31:14 +0000

    add deepspeed config

--- scripts/train_videollama_ds_lora.sh
+++ scripts/train_videollama_ds_lora.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 
-deepspeed --num_gpus=4 \
+# Environment Variables
+ARG_WORLD_SIZE=${1:-1}
+ARG_NPROC_PER_NODE=${1:-2}
+ARG_MASTER_ADDR="127.0.0.1"
+ARG_MASTER_PORT=16666
+ARG_RANK=${3:-0}
+
+# Multiple conditions
+if [ ! -n "$WORLD_SIZE" ] || [ ! -n "$NPROC_PER_NODE" ]; then
+    WORLD_SIZE=$ARG_WORLD_SIZE
+    NPROC_PER_NODE=$ARG_NPROC_PER_NODE
+fi
+if [ ! -n "$MASTER_ADDR" ] || [ ! -n "$MASTER_PORT" ] || [ ! -n "$RANK" ]; then
+    MASTER_ADDR=$ARG_MASTER_ADDR
+    MASTER_PORT=$ARG_MASTER_PORT
+    RANK=$ARG_RANK
+fi
+
+echo "WORLD_SIZE: $WORLD_SIZE"
+echo "NPROC_PER_NODE: $NPROC_PER_NODE"
+
+
+# Training Arguments
+GLOBAL_BATCH_SIZE=2
+LOCAL_BATCH_SIZE=1
+GRADIENT_ACCUMULATION_STEPS=$[$GLOBAL_BATCH_SIZE/($WORLD_SIZE*$NPROC_PER_NODE*$LOCAL_BATCH_SIZE)]
+
+torchrun --nnodes $WORLD_SIZE \
+    --nproc_per_node $NPROC_PER_NODE \
+    --master_addr=$MASTER_ADDR \
+    --master_port=$MASTER_PORT \
+    --node_rank $RANK \
     model/train_videollama.py \
     --deepspeed ds_config.json \
     --mamba_path "state-spaces/mamba-370m-hf" \
